36f24b84b5caa9d6761ceb753cad9fcb99d72584
Helder Lagrisola <helderlagrisola'gmail.com>
2025-05-02 07:43:47 +0100

    Fix print statements to use parentheses for Python 3 compatibility and add initial PHP file with a simple echo statement

--- Python/main.py
+++ Python/main.py
@@ -1,10 +1,10 @@
-print "Hello World" # This is a comment
- 
- #variable declaration
+print("Hello World")  # This is a comment
+
+# Variable declaration
 a = 10
 b = 20
 c = a + b
-print c # This will print 30
+print(c)  # This will print 30
 
 # Data Types
 # int, float, string, list, tuple, dictionary, set
@@ -13,60 +13,52 @@ print c # This will print 30
 a = 10
 b = 20
 c = a + b
-print c # This will print 30
-
+print(c)  # This will print 30
 
 # float
 a = 10.5
 b = 20.5
 c = a + b
-print c # This will print 31.0
-
+print(c)  # This will print 31.0
 
 # string
 a = "Hello"
 b = "World"
 c = a + " " + b
-print c # This will print Hello World
-
+print(c)  # This will print Hello World
 
 # list
 a = [1, 2, 3, 4, 5]
 b = [6, 7, 8, 9, 10]
 c = a + b
-print c # This will print [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
+print(c)  # This will print [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 # tuple
 a = (1, 2, 3, 4, 5)
 b = (6, 7, 8, 9, 10)
 c = a + b
-print c # This will print (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-
+print(c)  # This will print (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 
 # dictionary
 a = {"name": "John", "age": 30}
 b = {"name": "Jane", "age": 25}
 c = {**a, **b}
-print c # This will print {'name': 'Jane', 'age': 25} because the second dictionary overwrites the first one
-
+print(c)  # This will print {'name': 'Jane', 'age': 25}
 
 # set
 a = {1, 2, 3, 4, 5}
 b = {6, 7, 8, 9, 10}
 c = a.union(b)
-print c # This will print {1, 2, 3, 4, 5, 6, 7, 8, 9, 10} because sets are unordered collections of unique elements
-
+print(c)  # This will print {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 
 # Control Flow
 # if-else statement
 a = 10
 b = 20
 if a > b:
-    print "a is greater than b"
+    print("a is greater than b")
 elif a < b:
-    print "a is less than b"
+    print("a is less than b")
 else:
-    print "a is equal to b"
+    print("a is equal to b")
 
-    
